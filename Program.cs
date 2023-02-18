ded3b5d27d782bed7a93f5d33c1ac1d4554459d7
LAV <5958383@mail.ru>
2023-02-18 20:55:45 +0300

    Другой вариант решения задачи

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 ﻿
 string[] array = {"один", "two", "three", "four", "five", "six", "su", ".", " ", "2"};
 int i = 0, j = 0;
+Console.WriteLine("Построчно заполните массив по окончании нажмите Esc");
+do
+{
+    
+} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
 foreach (string item in array)
 {
     if (item.Length < 4) i++;
@@ -16,4 +22,4 @@ foreach (string item in array)
     }
 }
 Console.WriteLine($"Исходный массив: [{String.Join (",",array)}]");
-Console.WriteLine($"Массив содержащий строки в которых не более 3 символов[{String.Join (",",arr)}]");
+Console.WriteLine($"Массив содержащий строки в которых не более 3 символов: [{String.Join (",",arr)}]");
