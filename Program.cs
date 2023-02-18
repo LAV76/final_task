d42e9e3e9ad912ce86d21141bb328583d5bd7469
LAV <5958383@mail.ru>
2023-02-18 16:43:02 +0300

    Решение простым вариантом

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 ﻿
+string[] array = {"один", "two", "three", "four", "five", "six", "su", ".", " ", "2"};
+int i = 0, j = 0;
+foreach (string item in array)
+{
+    if (item.Length < 4) i++;
+}
+string[] arr = new string[i];
+
+foreach (string item in array)
+{
+    if (item.Length < 4) 
+    {
+        arr[j] = item;
+        j++;
+    }
+}
+Console.WriteLine($"[{String.Join (",",arr)}]");
